16abd607a48b0217117df73931ef675cb81216e4
Ashutosh Kumar Gautam Testbook <165718690+ashutoshkumargautamtb@users.noreply.github.com>
2024-09-09 17:37:46 +0530

    updated

--- Fresh/give-me-date.php
+++ Fresh/give-me-date.php
@@ -1,33 +1,5 @@
 <!------------------------------------------------------------------------------------------------------------>
 
-<?php
-require 'auth.php';  // Ensure session is active
-require './config/db.php'; // Include your database connection
-
-// Fetch the user booking details from the database
-$email = $_SESSION['email'];
-
-$stmt = $pdo->prepare("SELECT * FROM bookings WHERE email = ?");
-$stmt->execute([$email]);
-$user = $stmt->fetch();
-
-if ($user) {
-    // Retrieve details from the database
-    $name = $user['name'];
-    $email = $user['email'];
-    $phone = $user['phone'];
-    $date = $user['booking_date'];
-    $bookingIdx = $user['booking_id'];  // Retrieve `booking_id` from the database
-    $timeSlot = $user['slot'];
-} else {
-    echo "No booking found for this user.";
-    exit();
-}
-?>
-
-<?php
-session_destroy();
-?>
 
 <!------------------------------------------------------------------------------------------------------------>
 <!DOCTYPE html>
@@ -157,15 +129,9 @@ session_destroy();
 
     <!-- Success Message Container -->
     <div class="thank-you-container">
-        <img src="./img/green-tick.svg" alt="Success">
-        <h1>Seat Booked Successfully</h1>
+        <img src="./img/icons8-calendar-50.png" alt="Success">
         <div class="thank-you-details">
-            <p><span>Your Name:</span> <?php echo htmlspecialchars($name); ?></p>
-            <p><span>Your Booking ID:</span> <?php echo htmlspecialchars($bookingIdx); ?></p>
-            <p><span>Your Selected Time Slot:</span> <?php echo htmlspecialchars($timeSlot); ?></p>
-            <p><span>Your Email:</span> <?php echo htmlspecialchars($email); ?></p>
-            <p><span>Your Phone:</span> <?php echo htmlspecialchars($phone); ?></p>
-            <p><span>Booking Date:</span> <?php echo htmlspecialchars($date); ?></p>
+           
         </div>
         <a href="https://testbook.com/jee-main/test-series" target="_blank">Explore Full Test Series</a>
     </div>
