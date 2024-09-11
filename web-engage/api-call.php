b997cced3e5ba08e87024446257ce292d05ef67c
Ashutosh Kumar Gautam Testbook <165718690+ashutoshkumargautamtb@users.noreply.github.com>
2024-09-11 18:21:19 +0530

    updated

---
+++ web-engage/api-call.php
@@ -0,0 +1,9 @@
+<?php
+/*---------- This is PHP API Call --------------------*/
+echo "This api call for web engage : post new user journey.";
+/*---------------------------------------- user variables -----------------------------------*/
+/*---name, email, phone, booking_date, otp, slot, booking_id, varified - ?, created at - ? --*/
+
+// api url 
+
+?>
